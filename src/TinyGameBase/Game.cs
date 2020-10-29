e405a8e28fb39d0fc18141a8468c85e9375e29fd
Scott Holden <scott@nullops.io>
2020-10-29 22:05:45 +1100

    No XAML cleanup

--- src/TinyGameBase/Game.cs
+++ src/TinyGameBase/Game.cs
@@ -1,42 +1,19 @@
 ﻿using SkiaSharp;
-using System.Collections.Generic;
-using System.Windows.Documents;
 
 namespace TinyGameBase
 {
 	public class Game
 	{
-		private readonly List<Dot> _dots;
-
-		public Game()
-		{
-			_dots = new List<Dot>
-			{
-				new Dot(50,50),
-				new Dot(100,50),
-				new Dot(50,100),
-				new Dot(100,100)
-			};
-		}
-
 		// Called in 16ms steps
 		public void Update()
 		{
-			foreach (Dot d in _dots)
-			{
-				d.Update();
-			}
+
 		}
 
 		// Called as fast as posible, normally 60fps
 		public void Render(SKCanvas canvas, int width, int height)
 		{
 			canvas.Clear(SKColors.LightBlue);
-
-			foreach (Dot d in _dots)
-			{
-				d.Render(canvas);
-			}
 		}
 	}
 }
