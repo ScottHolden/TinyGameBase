6b673db1be7d399968ce3e5bcb5c766bccb42e16
Scott Holden <scott@nullops.io>
2020-10-29 22:12:25 +1100

    Moving dots

--- src/TinyGameBase/WPF/GameElement.cs
+++ src/TinyGameBase/WPF/GameElement.cs
@@ -8,82 +8,82 @@ using System.Windows.Media.Imaging;
 namespace TinyGameBase
 {
 	public class GameElement : FrameworkElement
-    {
-        private readonly bool _designMode;
-        private readonly Game _game;
-        private WriteableBitmap _bitmap;
-        private TimeSpan _lastRenderingTime = TimeSpan.Zero;
-        private TimeSpan _lastUpdateTime = TimeSpan.Zero;
-        private static readonly TimeSpan UpdateStepSize = TimeSpan.FromMilliseconds(16);
+	{
+		private readonly bool _designMode;
+		private readonly Game _game;
+		private WriteableBitmap _bitmap;
+		private TimeSpan _lastRenderingTime = TimeSpan.Zero;
+		private TimeSpan _lastUpdateTime = TimeSpan.Zero;
+		private static readonly TimeSpan UpdateStepSize = TimeSpan.FromMilliseconds(16);
 
-        public GameElement(Game game)
-        {
-            _designMode = DesignerProperties.GetIsInDesignMode(this);
-            _game = game;
-            CompositionTarget.Rendering += CompositionTargetRendering;
-            CompositionTarget.Rendering += GameUpdate;
-        }
+		public GameElement(Game game)
+		{
+			_designMode = DesignerProperties.GetIsInDesignMode(this);
+			_game = game;
+			CompositionTarget.Rendering += CompositionTargetRendering;
+			CompositionTarget.Rendering += GameUpdate;
+		}
 
-        private void GameUpdate(object sender, EventArgs e)
-        {
-            var args = (RenderingEventArgs)e;
-            
-            while (_lastUpdateTime < args.RenderingTime)
-            {
-                _game.Update();
-                _lastUpdateTime = _lastUpdateTime.Add(UpdateStepSize);
-            }
-        }
+		private void GameUpdate(object sender, EventArgs e)
+		{
+			var args = (RenderingEventArgs)e;
 
-        private void CompositionTargetRendering(object sender, EventArgs e)
-        {
-            var args = (RenderingEventArgs)e;
+			while (_lastUpdateTime < args.RenderingTime)
+			{
+				_game.Update();
+				_lastUpdateTime = _lastUpdateTime.Add(UpdateStepSize);
+			}
+		}
 
-            if (_lastRenderingTime == args.RenderingTime)
-            {
-                return;
-            }
+		private void CompositionTargetRendering(object sender, EventArgs e)
+		{
+			var args = (RenderingEventArgs)e;
 
-            _lastRenderingTime = args.RenderingTime;
+			if (_lastRenderingTime == args.RenderingTime)
+			{
+				return;
+			}
 
-            InvalidateVisual();
-        }
+			_lastRenderingTime = args.RenderingTime;
 
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            base.OnRender(drawingContext);
+			InvalidateVisual();
+		}
 
-            if (_designMode)
-                return;
+		protected override void OnRender(DrawingContext drawingContext)
+		{
+			base.OnRender(drawingContext);
 
-            int width = (int)this.ActualWidth;
-            int height = (int)this.ActualHeight;
+			if (_designMode)
+				return;
 
-            // Only resize if we need to
-            if (_bitmap == null || width != _bitmap.PixelWidth || height != _bitmap.PixelHeight)
-            {
-                _bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Pbgra32, null);
-            }
+			int width = (int)this.ActualWidth;
+			int height = (int)this.ActualHeight;
 
-            if (_bitmap == null)
-                return;
+			// Only resize if we need to
+			if (_bitmap == null || width != _bitmap.PixelWidth || height != _bitmap.PixelHeight)
+			{
+				_bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Pbgra32, null);
+			}
 
-            SKImageInfo info = new SKImageInfo(_bitmap.PixelWidth, _bitmap.PixelHeight, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
+			if (_bitmap == null)
+				return;
 
-            _bitmap.Lock();
+			SKImageInfo info = new SKImageInfo(_bitmap.PixelWidth, _bitmap.PixelHeight, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
 
-            // Render the game
-            using (var surface = SKSurface.Create(info, _bitmap.BackBuffer, _bitmap.BackBufferStride))
-            {
-                _game.Render(surface.Canvas, _bitmap.PixelWidth, _bitmap.PixelHeight);
-            }
+			_bitmap.Lock();
 
-            _bitmap.AddDirtyRect(new Int32Rect(0, 0, info.Width, info.Height));
+			// Render the game
+			using (var surface = SKSurface.Create(info, _bitmap.BackBuffer, _bitmap.BackBufferStride))
+			{
+				_game.Render(surface.Canvas, _bitmap.PixelWidth, _bitmap.PixelHeight);
+			}
 
-            _bitmap.Unlock();
+			_bitmap.AddDirtyRect(new Int32Rect(0, 0, info.Width, info.Height));
 
-            drawingContext.DrawImage(_bitmap, new Rect(0, 0, this.ActualWidth, this.ActualHeight));
+			_bitmap.Unlock();
 
-        }
-    }
+			drawingContext.DrawImage(_bitmap, new Rect(0, 0, this.ActualWidth, this.ActualHeight));
+
+		}
+	}
 }
